b9ff82bc2b2f090b76a61ad87666b9da8a2bdc19
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2023-03-18 21:52:28 -0500

    add callback function for debugging

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,50 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-
 use core::ffi::c_void;
 
 #[derive(Copy, Clone)]
 #[repr(C)]
 pub struct Allocator {
-    _container: [u8; core::mem::size_of::<&mut dyn MallocFree>()]
+    _container: [u8; core::mem::size_of::<&mut dyn MallocFree>()],
 }
 
 pub type AllocatorHandle = *const Allocator;
 
 pub type OnDropWithoutFree = fn();
 
-fn no_panic_on_drop_without_free(){}
+#[derive(Debug)]
+pub enum Action {
+    Free,
+    Malloc,
+    Error,
+}
+
+#[derive(Debug)]
+pub struct Status {
+    pub action: Action,
+    pub count: usize,
+    pub usage: usize,
+    pub maximum_usage: usize,
+}
+
+fn no_panic_on_drop_without_free() {}
 
 pub trait MallocFree {
-    fn malloc(self:&mut Self, size: usize) -> *mut c_void;
-    fn free(self:&mut Self, _ptr: *mut c_void);
+    fn malloc(self: &mut Self, size: usize) -> *mut c_void;
+    fn free(self: &mut Self, _ptr: *mut c_void);
     fn get_allocator(self: &mut Self) -> Allocator;
 }
 
 pub struct Bump<const SIZE: usize, const ALIGNMENT: usize> {
     count: usize,
     head: usize,
-    heap: [u8; SIZE],
+    pub heap: [u8; SIZE],
     maximum_usage: usize,
     on_drop_without_free: OnDropWithoutFree,
+    on_changed: Option<fn(Status)>,
 }
 
 impl<const SIZE: usize, const ALIGNMENT: usize> Bump<SIZE, ALIGNMENT> {
-
     pub fn new() -> Self {
         Self {
             count: 0,
@@ -38,6 +52,7 @@ impl<const SIZE: usize, const ALIGNMENT: usize> Bump<SIZE, ALIGNMENT> {
             heap: [0; SIZE],
             maximum_usage: 0,
             on_drop_without_free: no_panic_on_drop_without_free,
+            on_changed: None,
         }
     }
 
@@ -52,12 +67,29 @@ impl<const SIZE: usize, const ALIGNMENT: usize> Bump<SIZE, ALIGNMENT> {
     pub fn handle_drop_without_free(self: &mut Self, handler: OnDropWithoutFree) {
         self.on_drop_without_free = handler;
     }
+
+    pub fn handle_on_changed(self: &mut Self, handler: fn(Status)) {
+        self.on_changed = Some(handler);
+    }
+
+    fn changed(self: &Self, action: Action){
+        if let Some(handler) = self.on_changed {
+            handler(Status {
+                action,
+                count: self.count,
+                usage: self.head,
+                maximum_usage: self.maximum_usage
+            })
+        }
+    }
+
 }
 
 impl<const SIZE: usize, const ALIGNMENT: usize> MallocFree for Bump<SIZE, ALIGNMENT> {
-    fn malloc(self:&mut Self, size: usize) -> *mut c_void {
-        let next_head = self.head + ((size + ALIGNMENT -1) / ALIGNMENT) * ALIGNMENT;
+    fn malloc(self: &mut Self, size: usize) -> *mut c_void {
+        let next_head = self.head + ((size + ALIGNMENT - 1) / ALIGNMENT) * ALIGNMENT;
         if next_head > SIZE {
+            self.changed(Action::Error);
             return core::ptr::null_mut();
         }
         let result = &mut self.heap[self.head] as *mut u8;
@@ -66,10 +98,11 @@ impl<const SIZE: usize, const ALIGNMENT: usize> MallocFree for Bump<SIZE, ALIGNM
             self.maximum_usage = self.head;
         }
         self.count = self.count + 1;
+        self.changed(Action::Malloc);
         result as *mut c_void
     }
 
-    fn free(self:&mut Self, _ptr: *mut c_void){
+    fn free(self: &mut Self, _ptr: *mut c_void) {
         //if no items are used, reset the head
         if self.count > 0 {
             self.count = self.count - 1;
@@ -77,12 +110,11 @@ impl<const SIZE: usize, const ALIGNMENT: usize> MallocFree for Bump<SIZE, ALIGNM
                 self.head = 0;
             }
         }
+        self.changed(Action::Free);
     }
 
     fn get_allocator(self: &mut Self) -> Allocator {
-        unsafe {
-            core::mem::transmute(self as &mut dyn MallocFree)
-        }
+        unsafe { core::mem::transmute(self as &mut dyn MallocFree) }
     }
 }
 
@@ -93,7 +125,7 @@ impl Allocator {
 }
 
 impl<const SIZE: usize, const ALIGNMENT: usize> Drop for Bump<SIZE, ALIGNMENT> {
-    fn drop(self: &mut Self){
+    fn drop(self: &mut Self) {
         if self.count > 0 {
             (self.on_drop_without_free)();
         }
@@ -128,7 +160,4 @@ mod tests {
         bump.free(first);
         assert_eq!(bottom_of_heap, get_location(&bump, bump.head));
     }
-
-
-
 }
